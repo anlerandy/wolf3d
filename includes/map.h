08bd96e700e3b80e70ab611521613b4296477573
acourtin <acourtin@student.42.fr>
2018-02-18 16:48:31 +0100

    ajout d'un calculateur de taille de map

--- includes/map.h
+++ includes/map.h
@@ -6,30 +6,39 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 14:08:23 by acourtin          #+#    #+#             */
-/*   Updated: 2018/02/18 14:08:29 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/18 16:44:45 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_H
 # define MAP_H
 
-typedef enum		e_tile_type
+typedef enum		e_texture
 {
-	FLOOR,
-	WALL,
+	BOIS,
+	PIERRE,
+	METAL
+}					t_texture;
+
+typedef enum		e_entity
+{
+	NONE,
 	PLAYER_START
-}					t_tile_type;
+}					t_entity;
 
 typedef struct		s_tile
 {
-	t_tile_type		type;
-	int				collision;
+	int				height;
+	t_texture		texture;
+	t_entity		entity;
 }					t_tile;
 
 typedef struct		s_map
 {
 	char			*name;
-	t_tile			tiles[64][64];
+	t_tile			**tiles;
+	int				width;
+	int				height;
 }					t_map;
 
 #endif
--- srcs/map.c
+++ srcs/map.c
@@ -6,36 +6,40 @@
 /*   By: alerandy <alerandy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/17 21:04:20 by alerandy          #+#    #+#             */
-/*   Updated: 2018/02/18 14:40:05 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/18 16:47:19 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-static int		verify_file(t_data *data, char *filename)
+static void		get_size(t_data *data, char *line, int *w, int *h)
+{
+	*h += 1;
+	if (*w < ft_strlen(line))
+		*w = ft_strlen(line);
+}
+
+static int		verify_file(t_data *data, char *filename, int *w, int *h)
 {
 	int		fd;
-	int		count;
-	int		count_j;
 	char	*line;
+	int		step;
 
 	if ((fd = open(filename, O_RDONLY)) > 0)
 	{
-		count = 0;
-		count_j = 0;
+		step = 0;
 		while (get_next_line(fd, &line) == 1)
 		{
-			if (ft_strlen(line) > 64)
-				return (0);
-			if (ft_strchr(line, 'J'))
-				count_j++;
+			if (ft_strcmp(line, "TEXTURE:") == 0)
+				step = 1;
+			else if (ft_strcmp(line, "HEIGHT:") == 0)
+				break ;
+			else if (step == 1)
+				get_size(data, line, w, h);
 			ft_strdel(&line);
-			count++;
 		}
 		ft_strdel(&line);
 		close(fd);
-		if (count > 64 || count_j != 1)
-			return (0);
 	}
 	else
 		return (0);
@@ -45,19 +49,23 @@ static int		verify_file(t_data *data, char *filename)
 void			map(t_data *data, int map)
 {
 	int i;
+	int h;
+	int w;
 
 	i = -1;
+	h = 0;
+	w = 0;
 	debug(data->debug, "Chargement de la map : ");
 	debug(data->debug, ft_itoa(map + 1));
 	sleep(1);
 	data->loading = 0;
 	data->game_state = GAME;
 	data->map.name = ft_strjoin("maps/map", ft_itoa(map + 1));
-	while (++i <= 64 * 64)
-	{
-		data->map.tiles[i / 64][i % 64].type = FLOOR;
-		data->map.tiles[i / 64][i % 64].collision = 0;
-	}
-	if (verify_file(data, data->map.name) == 0)
+	if (verify_file(data, data->map.name, &w, &h) == 0)
 		usage(4);
+	data->map.width = w;
+	data->map.height = h;
+	data->map.tiles = ft_memalloc(sizeof(int*) * h);
+	while (++i < h)
+		data->map.tiles[i] = ft_memalloc(sizeof(int) * w);
 }
