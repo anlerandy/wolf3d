996b900f99043c2bd6961f1cef98633f53e5a2dd
acourtin <acourtin@student.42.fr>
2018-02-21 13:49:31 +0100

    le joueur peut maintenant tourner la camera avec les fleches

--- includes/player.h
+++ includes/player.h
@@ -6,12 +6,14 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 21:03:04 by acourtin          #+#    #+#             */
-/*   Updated: 2018/02/21 12:39:10 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/21 13:45:28 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PLAYER_H
 # define PLAYER_H
+
+# define PLAYER_ROT 10
 
 typedef struct			s_pos
 {
