8fa8ca545388e60ffb8737be6374e8cc8b723a26
acourtin <acourtin@student.42.fr>
2018-02-23 20:27:36 +0100

    Ajout de differente couleurs pour les murs selon la position du joueur

--- srcs/world.c
+++ srcs/world.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 17:18:38 by acourtin          #+#    #+#             */
-/*   Updated: 2018/02/23 19:29:27 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/23 20:25:19 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,21 @@ void		draw_wall(t_data *data, t_ray ray, int slice)
 	double	h;
 	double	d;
 	double	h2;
+	t_ray	dist;
 	int		color;
 
 	h = 0;
 	h2 = 1;
-	d = (100 - ray.depth) * 4;
-	color = 0x00010000 * (d / 2);
-	while (h < d * 0.8)
+	dist.x = data->player.pos.x - ray.x;
+	dist.y = data->player.pos.y - ray.y;
+	dist.x > 0 && dist.y > 0 ? color = 0x00000001 : 0;
+	dist.y > 0 && dist.y < 0 ? color = 0x00000100 : 0;
+	dist.y < 0 && dist.y < 0 ? color = 0x00010000 : 0;
+	dist.x < 0 && dist.y > 0 ? color = 0x00010100 : 0;
+	color *= ((100 - ray.depth) * 4) / 2;
+	d = sqrt(dist.x * dist.x + dist.y * dist.y);
+	d = (100 - d) * 2;
+	while (h < d)
 	{
 		((int*)data->frame.img)[1400 - slice + (((int)(data->win_h / 2) \
 				+ (int)h) * data->win_w)] = color;
