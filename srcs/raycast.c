3d476528bdd233c02af4cd0254392e9dbb4ef38a
acourtin <acourtin@student.42.fr>
2018-02-23 16:18:58 +0100

    Ajout d'une nouvelle map de test. Correction de la position du joueur incorrecte sur la minimap. Correction de la collision des rayons incorrecte

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 20:44:58 by acourtin          #+#    #+#             */
-/*   Updated: 2018/02/22 23:31:31 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/23 16:06:33 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,26 @@
 
 static void		loop_ray(t_data *data, t_ray *r, double rotx, double roty)
 {
-	((int*)data->minimap.img)[(int)((r->x * 3) + 2.5) + ((int)((r->y * 3) + 2.5) * 300)] = 0x0000FF00;
-	r->x += rotx;
-	r->y += roty;
+	((int*)data->minimap.img)[(int)((r->x * 3)) + ((int)((r->y * 3)) * 300)] = 0x0000FF00;
+	r->x += rotx / 2;
+	r->y += roty / 2;
 	//r->x = floor(r->x);
 	//r->y = floor(r->y);
 }
 
 static int		check_z(t_data *data, t_ray *r, double rotx, double roty)
 {
-	if (data->map.tiles[(int)(r->y / 1.3)][(int)(r->x / 1.3)].z > 0)
+	if (data->map.tiles[(int)((r->y / 1.335))][(int)((r->x / 1.335))].z > 0)
 		return (1);
 	return (0);
 }
 
 void			send_ray(t_data *data, t_ray *r, double rotx, double roty)
 {
-	r->x = data->player.pos.x;
-	r->y = data->player.pos.y;
+	r->x = data->player.pos.x + 1.0;
+	r->y = data->player.pos.y + 1.0;
 	while ((r->x > 0 && r->x < 99 && r->y > 0 && r->y < 99) && check_z(data, r, rotx, roty) == 0)
-	//while (r->x > 0 && r->x < 99 && r->y > 0 && r->y < 99)
+	//while (r->x > 0 && r->x < 100 && r->y > 0 && r->y < 100)
 		loop_ray(data, r, rotx, roty);
 	if (r->x < 0)
 		r->x = 0;
