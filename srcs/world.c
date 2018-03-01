f4792dcb989f59840d684b52d0f429b75fca24fc
acourtin <acourtin@student.42.fr>
2018-03-01 03:41:52 +0100

    ajout d'une texture de mur de pierre

--- srcs/world.c
+++ srcs/world.c
@@ -47,7 +47,13 @@ void		draw_wall(t_data *data, t_ray ray, int slice)
 	double	h2;
 	t_ray	dist;
 	int		color;
+	int		color2;
+	static t_xpm tex;
+	static int s = 0;
 
+	if (!s)
+		tex = xpm_create(data, "./xpm/stone.xpm", 1400, 800);
+	s = 1;
 	h = 0;
 	h2 = 1;
 	dist.x = data->player.pos.x - ray.x;
@@ -62,12 +68,22 @@ void		draw_wall(t_data *data, t_ray ray, int slice)
 	ray.dir == EAST ? color = 0x0000FF00 : color;
 	ray.dir == NORTH ? color = 0x000000FF : color;
 	ray.dir == SOUTH ? color = 0x00FFFF00 : color;
+	color2 = color;
 	while (h < d)
 	{
-			((int*)data->frame.img)[1400 - slice + (((int)(data->win_h / 2) \
-				+ (int)h) * data->win_w)] = color;
-			((int*)data->frame.img)[1400 - slice + (((int)(data->win_h / 2) \
-				- (int)h2) * data->win_w)] = color;
+		//color = tex.img[(int)((ray.x - (int)ray.x) * 1400 + (400 - h) * 400 / d + (h * 1400))];
+		//color2 = tex.img[(int)((ray.x - (int)ray.x) * 1400 + (400 + (h + 1)) * 400 / d + (h * 1400))];
+		if (ray.dir == NORTH)
+		{
+		if ((int)(slice + (((h * 200 / d) + (h * 200 / d)) * 1400)) < 1400 * 800)
+			color = tex.img[(int)(slice + (((h * 200 / d) + (h * 200 / d)) * 1400))];
+		//if ((int)(slice + ((((h + 1) * 200 / d) + ((h + 1) * 200 / d)) * 1400)) < 1400 * 800)
+		//	color2 = tex.img[(int)(slice + ((((h + 1) * 200 / d) + ((h + 1) * 200 / d)) * 1400))];
+		}
+		((int*)data->frame.img)[1400 - slice + (((int)(data->win_h / 2) \
+			- (int)h) * data->win_w)] = color;
+		((int*)data->frame.img)[1400 - slice + (((int)(data->win_h / 2) \
+			+ (int)(h + 1)) * data->win_w)] = color2;
 		h += 1;
 		h2 += 1;
 	}
