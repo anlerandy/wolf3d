2553ac24057a8e3285078f011c3fa7166e859b60
acourtin <acourtin@student.42.fr>
2018-03-02 16:38:34 +0100

    correction du petit curseur vert qui affiche la rotation du joueur sur la minimap

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 10:51:51 by acourtin          #+#    #+#             */
-/*   Updated: 2018/03/01 07:46:10 by alerandy         ###   ########.fr       */
+/*   Updated: 2018/03/02 16:36:21 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ static void		draw_player(t_data *data, double x, double y)
 	i = -1;
 	if (!data->debug)
 		while (++i < 3 * 3)
-			((int*)data->minimap.img)[(int)((x * 3 + (i % 3) + \
-					(data->player.rotx * 3)) + ((y * 3 + (i / 3) + \
-							(data->player.roty * 3)) * 300))] = 0x0000FF00;
+			((int*)data->minimap.img)[((int)((x - 1.25 + data->player.rotx) \
+				* 4 + (i % 3))) + (((int)((y - 1.25 + data->player.roty) * 4 \
+				+ (i / 3))) * 300)] = 0x0000FF00;
 }
 
 static void		draw_cube(t_data *data, int x, int y)
