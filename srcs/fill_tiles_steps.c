de31c97cc88e419f4d0d31efe99327d167af5540
Andy LERANDY <alerandy@student.42.fr>
2018-03-07 04:41:33 +0100

    Sauvegarde simple.

--- srcs/fill_tiles_steps.c
+++ srcs/fill_tiles_steps.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/18 17:13:37 by acourtin          #+#    #+#             */
-/*   Updated: 2018/03/07 04:03:09 by alerandy         ###   ########.fr       */
+/*   Updated: 2018/03/07 04:33:22 by alerandy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,8 +85,8 @@ static void	player_start(t_data *data, int i, char **tab)
 		usage(14);
 	if (data->map.tiles[y][x].z == 0)
 	{
-		data->player.pos.x = x + 0.50001;
-		data->player.pos.y = y + 0.50001;
+		data->player.pos.x = x + 0.5;
+		data->player.pos.y = y + 0.5;
 		if (i == 4)
 			if (ft_atoi(tab[3]) >= 0 && ft_atoi(tab[3]) < 360)
 				data->player.rot = ft_atoi(tab[3]);
